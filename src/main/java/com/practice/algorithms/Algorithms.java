142ec8eed1e7440252f166a3e6d9702d118825f0
Jonathon Degn <jonathon.degn@domo.com>
2017-07-06 20:03:32 -0600

    Add TwoSum and ThreeSum

--- src/main/java/com/practice/algorithms/Algorithms.java
+++ src/main/java/com/practice/algorithms/Algorithms.java
@@ -1,6 +1,9 @@
 package com.practice.algorithms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by jonathondegn on 6/29/17.
@@ -46,4 +49,76 @@ public class Algorithms {
         } while (nums[i] + nums[j] != target);
         return new int[]{i + 1, j + 1};
     }
+
+    // Two Sum III - Data structure design
+    //
+    // Design and implement a TwoSum class. It should support the following operations:
+    // add and find.
+    // add - Add the number to an internal data structure. find - Find if there exists any
+    // pair of numbers which sum is equal to the value.
+    public class TwoSum {
+        HashMap<Integer, Integer> numbers = new HashMap<>();
+
+        public void add(int num) {
+            if (numbers.containsKey(num)) {
+                numbers.put(num, numbers.get(num) + 1);
+            } else {
+                numbers.put(num, 1);
+            }
+        }
+
+        public boolean find(int num) {
+            for (int i : numbers.keySet()) {
+                int target = num - i;
+                if (numbers.containsKey(target)) {
+                    if (i == target && numbers.get(target) < 2) {
+                        continue;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    // Three Sum
+    //
+    // Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+    // Find all unique triplets in the array which gives the sum of zero.
+    // Note: Elements in a triplet (a,b,c) must be in non-descending order. (ie, a  b  c)
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || nums[i] > nums[i - 1]) {
+                int j = i + 1;
+                int k = nums.length - 1;
+
+                while (j < k) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        j++;
+                        k--;
+
+                        while (j < k && nums[j] == nums[j - 1])
+                            j++;
+                        while (j < k && nums[k] == nums[k + 1])
+                            k--;
+                    } else if (nums[i] + nums[j] + nums[k] < 0) {
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+    // O(n^2)
 }
--- src/test/java/com/practice/algorithms/AlgorithmsTest.java
+++ src/test/java/com/practice/algorithms/AlgorithmsTest.java
@@ -5,6 +5,9 @@ import com.practice.linkedlist.LinkedList;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AlgorithmsTest {
 
     @Test
@@ -29,5 +32,30 @@ public class AlgorithmsTest {
         given = new int[]{2, 3, 4};
         target = 6;
         Assert.assertArrayEquals(new int[]{1, 3}, Algorithms.twoSumII(given, target));
+    }
+
+    @Test
+    public void testTwoSumIII() {
+        Algorithms a = new Algorithms();
+
+        Algorithms.TwoSum twoSum = a.new TwoSum();
+        twoSum.add(2);
+        twoSum.add(3);
+        twoSum.add(4);
+        twoSum.add(7);
+        twoSum.add(11);
+        twoSum.add(15);
+        Assert.assertTrue(twoSum.find(9));
+        Assert.assertTrue(twoSum.find(6));
+        Assert.assertFalse(twoSum.find(1));
+        Assert.assertFalse(twoSum.find(100));
+        Assert.assertFalse(twoSum.find(8));
+    }
+
+    @Test
+    public void testThreeSum() {
+        List<List<Integer>> result = Algorithms.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        Assert.assertArrayEquals(new Integer[]{-1, -1, 2}, result.get(0).toArray());
+        Assert.assertArrayEquals(new Integer[]{-1, 0, 1}, result.get(1).toArray());
     }
 }
