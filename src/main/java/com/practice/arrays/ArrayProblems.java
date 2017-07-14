f2c8ee040adaf0baa393b25fd001d6630350493a
Jonathon Degn <jonathon.degn@domo.com>
2017-07-13 21:57:04 -0600

    Add moveZeroes

--- src/main/java/com/practice/arrays/ArrayProblems.java
+++ src/main/java/com/practice/arrays/ArrayProblems.java
@@ -20,4 +20,24 @@ public class ArrayProblems {
         }
         return Arrays.copyOf(a, idxToReplace);
     }
+
+    //    Given an array nums, write a function to move all 0’s to the end of it while maintaining
+    //    the relative order of the non-zero elements.
+    public static void moveZeroes(int[] nums) {
+        int idxToScan = 0, idxToReplace = 0;
+
+        while (idxToScan < nums.length) {
+            if (nums[idxToScan] == 0) {
+                idxToScan++;
+            } else {
+                nums[idxToReplace] = nums[idxToScan];
+                idxToReplace++;
+                idxToScan++;
+            }
+        }
+        while (idxToReplace < nums.length) {
+            nums[idxToReplace] = 0;
+            idxToReplace++;
+        }
+    }
 }
--- src/test/java/com/practice/arrays/ArrayProblemsTest.java
+++ src/test/java/com/practice/arrays/ArrayProblemsTest.java
@@ -12,7 +12,14 @@ public class ArrayProblemsTest {
     @Test
     public void testRemoveElement() {
         int[] result = ArrayProblems.removeElement(new int[]{1, 4, 6, 5, 2, 3, 7, 1, 1}, 1);
-        Assert.assertArrayEquals(new int[] {4,6,5,2,3,7}, result);
+        Assert.assertArrayEquals(new int[]{4, 6, 5, 2, 3, 7}, result);
+    }
+
+    @Test
+    public void testMoveZeroes() {
+        int[] nums = {0, 1, 0, 3, 12};
+        ArrayProblems.moveZeroes(nums);
+        Assert.assertArrayEquals(new int[]{1, 3, 12, 0, 0}, nums);
     }
 
 }
